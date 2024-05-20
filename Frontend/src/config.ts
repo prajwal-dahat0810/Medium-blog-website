11a735f0a1fe6a816a0286494d709e1335fdfa61
Prajwal <130838578+prajwal-dahat0810@users.noreply.github.com>
2024-05-20 22:26:05 +0530

    Update config.ts

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -1,3 +1,2 @@
-// export const BACKEND_URL = "https://backend.dahatprajwal19.workers.dev"
-// export const BACKEND_URL = "https://localhost:3000"
-export const BACKEND_URL = "http://127.0.0.1:8787"
+
+export const BACKEND_URL = ""
